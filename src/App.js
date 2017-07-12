42c128bbe4fea8c041a213043c226e50598279ac
deks <deks@MacBook-Air-deks.local>
2017-07-12 15:19:52 +0200

    Dodana strona szczegolowa dla dalego miasta
    Strona pobiera dane z api i wyswietla czesc z nich.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Router, Route, IndexRoute, Link, hashHistory } from "react-router";
 import { Search } from "./Search";
 import {Home} from "./Home";
-
+import {WeatherDetails} from "./Weather/WeatherDetails"
 class App extends Component {
   render() {
     return (
@@ -10,7 +10,7 @@ class App extends Component {
         <Router history={hashHistory}>
           <Route path="/" component={Search}>
             <IndexRoute component={Home} />
-
+            <Route component={WeatherDetails} path="weatherdetails"/>
           </Route>
         </Router>
       </div>
--- src/lib/api-client.js
+++ src/lib/api-client.js
@@ -1,14 +1,11 @@
+
 import axios from "axios";
 
-const APIKEY ="057420ef754b30b2b45ecd0d40b6281d"
+const APIKEY ="057420ef754b30b2b45ecd0d40b6281d";
+
 var apiClient = axios.create({
-  baseURL: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}"
+  baseURL: "http://api.openweathermap.org/data/2.5",
+  params: { "APPID": APIKEY }
 });
-
-
-
-
-
-
 
 export default apiClient;
