e913d8803585e0331872dcf971def5a8716cf014
Maciek <maciejostojski4@gmail.com>
2017-07-12 13:07:11 +0200

    Refactor i podpięty apiClient pod komponent WeatherAggregator

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component, PropTypes } from "react";
 import WeatherAggregator from "./weather/WeatherAggregator";
 
 export class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cityIds: [
+        6695624,
+        2988507,
+        3117735,
+        2643743,
+        2950159,
+        703448,
+        524901,
+        2759794,
+        3143244,
+        6458923,
+      ],
+    };
+  }
+
   render() {
-    return (
-      <WeatherAggregator/>
-    );
+    return <WeatherAggregator cityIds={this.state.cityIds} />;
   }
 }
 
--- src/weather/WeatherAggregator.js
+++ src/weather/WeatherAggregator.js
@@ -2,7 +2,7 @@
  * Created by react on 12.07.17.
  */
 import React from "react";
-import axios from "axios";
+import apiClient from "../lib/api-client";
 import CityWeather from "./city/CityWeather";
 
 class WeatherAggregator extends React.Component {
@@ -11,33 +11,23 @@ class WeatherAggregator extends React.Component {
 
     this.state = {
       cities: [],
-      cityIds: [
-        6695624,
-        2988507,
-        3117735,
-        2643743,
-        2950159,
-        703448,
-        524901,
-        2759794,
-        3143244,
-        6458923,
-      ],
     };
   }
 
   fetchWeather = () => {
-    let urlWithIds =
-      WEATHER_FOR_CITIES_URL + "&id=" + this.state.cityIds.join(",");
-    axios
-      .get(urlWithIds)
+    const urlWithCitiesIds =
+      WEATHER_FOR_SEVERAL_CITIES_URL + "&id=" + this.props.cityIds.join(",");
+    apiClient
+      .get(urlWithCitiesIds)
       .then(response => {
         this.setState({
           cities: response.data.list,
         });
       })
       .catch(error => {
-        console.log(error);
+        console.log(
+          "Error occurred during fetching weather for cities: " + error,
+        );
       });
   };
 
@@ -52,10 +42,8 @@ class WeatherAggregator extends React.Component {
           <table className="table table-bordered">
             <tbody>
               <tr>
-                {this.state.cities.map((city, index) => {
-                  return (
-                    <CityWeather city={city} />
-                  )
+                {this.state.cities.map(city => {
+                  return <CityWeather city={city} />;
                 })}
               </tr>
             </tbody>
@@ -66,7 +54,6 @@ class WeatherAggregator extends React.Component {
   }
 }
 
-const WEATHER_FOR_CITIES_URL =
-  "http://api.openweathermap.org/data/2.5/group?units=metric&APPID=9aba8198a425a0c685d4df40f8a35b16";
+const WEATHER_FOR_SEVERAL_CITIES_URL = "/group?units=metric";
 
 export default WeatherAggregator;
