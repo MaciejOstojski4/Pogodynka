2487deb7004ab148bf01adecba6f8e72f41f8a6b
Maciek <maciejostojski4@gmail.com>
2017-07-13 12:03:27 +0200

    Refactor Search komponentu

--- src/Search.js
+++ src/Search.js
@@ -1,25 +1,73 @@
-import React, { Component, PropTypes } from "react";
-import { Link } from "react-router";
+import React, { Component } from "react";
 import ReactHover from "react-hover";
 import InfoPopup from "./user-interface/InfoPopup";
+import apiClient from "./lib/api-client";
 
 export class Search extends Component {
-  onSubmit = e => {
-    e.preventDefault();
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputText: "",
+    };
+  }
+
+  isSearchingByCityName = () => {
+    if (this.state.inputText.match(LAT_LONG_REGEX)) {
+      return false;
+    }
+    return true;
   };
 
-  showPopup = () => {};
+  refreshState = e => {
+    e.preventDefault();
+    this.setState({
+      inputText: e.target.value,
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.isSearchingByCityName()) {
+      const urlForCityName = `${SEARCH_URL}q=${this.state.inputText}`;
+      apiClient
+        .get(urlForCityName)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log("Error while searching by city name: " + error);
+        });
+    } else {
+      const latAndLongValue = this.state.inputText.split(":");
+      const urlForLatAndLong = `${SEARCH_URL}lat=${latAndLongValue[0]}&lon=${latAndLongValue[1]}`;
+      apiClient
+        .get(urlForLatAndLong)
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(
+            "Error while searching by latitude and longitude: " + error,
+          );
+        });
+    }
+  };
 
   render() {
     return (
       <div className="row">
         <div className="col-md-4 col-md-offset-4 text-center">
-          <form>
+          <form className="form-inline">
             <div className="form-group">
-              <label>Search</label>
               <ReactHover options={REACT_HOVER_OPTS}>
                 <ReactHover.Trigger>
-                  <input className="form-control" placeholder="Search" />
+                  <input
+                    className="form-control"
+                    placeholder="Search"
+                    type="text"
+                    onChange={this.refreshState}
+                  />
                 </ReactHover.Trigger>
                 <ReactHover.Hover>
                   <InfoPopup text={SEARCH_INPUT_POPUP_TEXT} />
@@ -27,8 +75,12 @@ export class Search extends Component {
               </ReactHover>
             </div>
             <div className="form-group">
-              <button onClick={this.onSubmit} onMouseOver={this.showPopup}>
-                Ok
+              <button
+                className="btn btn-md"
+                type="submit"
+                onClick={this.onSubmit}
+              >
+                Search
               </button>
             </div>
           </form>
@@ -45,6 +97,10 @@ const REACT_HOVER_OPTS = {
 };
 
 const SEARCH_INPUT_POPUP_TEXT =
-  "Search by city name or by latitude and longitude (lat:lon)";
+  "Type city name or lat:lon values";
+
+const SEARCH_URL = "/weather?units=metric&";
+
+const LAT_LONG_REGEX = /(-)?[0-9]+\.[0-9]+:(-)?[0-9]+\.[0-9]+/;
 
 export default Search;
