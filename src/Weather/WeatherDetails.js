bfa592ceda22c139774be943cfb35e262cfbcb0b
deks <deks@MacBook-Air-deks.local>
2017-07-13 10:47:12 +0200

    Dodanie WeatherDetails do mastera

--- src/Weather/WeatherDetails.js
+++ src/Weather/WeatherDetails.js
@@ -2,90 +2,127 @@ import React, { Component, PropTypes } from "react";
 import OpenWeatherMap from "react-open-weather-map";
 import axios from "axios";
 import apiClient from "../lib/api-client";
+import styled from "styled-components";
 export class WeatherDetails extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      city: "Warsaw",
-      apiKey: "057420ef754b30b2b45ecd0d40b6281d",
-      url: "api.openweathermap.org/data/2.5/weather?q=",
-      d: [],
-      temp: []
+      cityId: 3093133,
+      data: "",
+      temp: ""
     };
   }
 
   fetchWeather = () => {
-    /*
-    const urlWithCitiesIds =
-      WEATHER_FOR_SINGLE_CITY_URL + "&q=" + this.state.city;
-      */
-    axios
-      .get(
-        `http://api.openweathermap.org/data/2.5/weather?q={${this.state
-          .city}}&APPID=057420ef754b30b2b45ecd0d40b6281d`
-      )
+    const urlWithCityId = `${WEATHER_FOR_SINGLE_CITY_URL}id=${this.state
+      .cityId}`;
+
+    apiClient
+      .get(urlWithCityId)
       .then(response => {
         this.setState({
-          d: response.data,
+          data: response.data,
           temp: response.data.main
         });
-        console.log(response.data);
-
-        /*console.log(response);*/
       })
       .catch(error => {
-        console.log(
-          "Error occurred during fetching weather for cities: " + error
-        );
+        console.log(error);
       });
   };
-  /*
-  apiClient
-    .get(urlWithCitiesIds)
-    .then(response => {
-      this.setState={
-        d: response.data.list,
-      };
-    })
-    .catch(error => {
-      console.log(
-        "Error occurred during fetching weather for cities: " + error,
-      );
-    });
-    console.log(this.state.d);
-};
-*/
+
   componentDidMount() {
     this.fetchWeather();
-  };
+  }
 
   render() {
-      console.log(this.state.temp.temp);
+
+    const pStyle = {
+      width: "500px",
+      paddingTop: "20px",
+      paddingLeft: "100px"
+    };
+    const boxStyle = {
+      backgroundColor: "#faebd7"
+    };
+    const hss = {
+      paddingTop: "20px",
+      paddingLeft: "20px"
+    };
+
+    console.log(this.state.data);
     return (
-      <div>
-        <ul>
-          <li>
-            City: {this.state.d.name}
-          </li>
-        <li>
-          Temp: {this.state.temp.temp}
-        </li>
-        <li>
-          Max temperature: {this.state.temp.temp_max}
-        </li>
-        <li>
-          Min temperature: {this.state.temp.temp_min}
-        </li>
-        <li>
-          Pressure: {this.state.temp.pressure}
-        </li>
-        </ul>
-      </div>
+      <Div className="container-fluid">
+        <h1>
+          {this.state.data.name}
+          <p>
+            <small> Weather details</small>
+          </p>
+        </h1>
+        <p style={pStyle}>
+          <div style={boxStyle} className="card">
+            <div className="card-header">
+              <h3 style={hss}>Temperature:</h3>
+            </div>
+            <div class="card-block">
+              <blockquote class="card-blockquote">
+
+                  {this.state.temp.temp}
+
+              </blockquote>
+            </div>
+          </div>
+
+          <div style={boxStyle} className="card">
+            <div className="card-header">
+              <h3 style={hss}>Max temperature:</h3>
+            </div>
+            <div class="card-block">
+              <blockquote class="card-blockquote">
+                {this.state.temp.temp_max}
+              </blockquote>
+            </div>
+          </div>
+
+          <div style={boxStyle} className="card">
+            <div className="card-header">
+              <h3 style={hss}>Min temperature:</h3>
+            </div>
+            <div class="card-block">
+              <blockquote class="card-blockquote">
+                {this.state.temp.temp_min}
+              </blockquote>
+            </div>
+          </div>
+
+          <div style={boxStyle} className="card">
+            <div className="card-header">
+              <h3 style={hss}>Pressure: </h3>
+            </div>
+            <div class="card-block">
+              <blockquote class="card-blockquote">
+                {this.state.temp.pressure}
+              </blockquote>
+            </div>
+          </div>
+
+          <div style={boxStyle} className="card">
+            <div className="card-header">
+              <h3 style={hss}>Humidity:</h3>
+            </div>
+            <div class="card-block">
+              <blockquote class="card-blockquote">
+                {this.state.temp.humidity}
+              </blockquote>
+            </div>
+          </div>
+        </p>
+      </Div>
     );
   }
 }
-
-const WEATHER_FOR_SINGLE_CITY_URL = "/weather?";
+const Div=styled.div`
+  display: flex
+`
+const WEATHER_FOR_SINGLE_CITY_URL = "/weather?units=metric&";
 
 export default WeatherDetails;
