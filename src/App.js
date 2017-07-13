bdd2e6fa12da744a8da0f37e371dd9d0ff703f81
Maciek <maciejostojski4@gmail.com>
2017-07-13 11:20:13 +0200

    Popięty komponent InfoPopup do seach inputa, wyświetla się po najechaniu na niego

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import { Router, Route, IndexRoute, Link, hashHistory } from "react-router";
-import { Search } from "./Search";
-import {Home} from "./Home";
-import {WeatherDetails} from "./Weather/WeatherDetails"
+import { Home } from "./Home";
+import Layout from "./Layout";
+import { WeatherDetails } from "./weather/WeatherDetails";
+
 class App extends Component {
   render() {
     return (
       <div className="container">
         <Router history={hashHistory}>
-          <Route path="/" component={Search}>
+          <Route path="/" component={Layout}>
             <IndexRoute component={Home} />
-
-            <Route component={WeatherDetails} path="weatherdetails"/>
-
+            <Route component={WeatherDetails} path="weatherdetails" />
           </Route>
         </Router>
       </div>
--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,50 @@
 import React, { Component, PropTypes } from "react";
 import { Link } from "react-router";
+import ReactHover from "react-hover";
+import InfoPopup from "./user-interface/InfoPopup";
 
 export class Search extends Component {
+  onSubmit = e => {
+    e.preventDefault();
+  };
+
+  showPopup = () => {};
+
   render() {
     return (
-      <div>
-        <div>Search</div>
-        <div><Link to="/weatherdetails">det</Link></div>
-
-        <div className="container">
-          {React.Children.map(this.props.children, c =>
-            React.cloneElement(c, { clickPage: () => {} })
-          )}
+      <div className="row">
+        <div className="col-md-4 col-md-offset-4 text-center">
+          <form>
+            <div className="form-group">
+              <label>Search</label>
+              <ReactHover options={REACT_HOVER_OPTS}>
+                <ReactHover.Trigger>
+                  <input className="form-control" placeholder="Search" />
+                </ReactHover.Trigger>
+                <ReactHover.Hover>
+                  <InfoPopup text={SEARCH_INPUT_POPUP_TEXT} />
+                </ReactHover.Hover>
+              </ReactHover>
+            </div>
+            <div className="form-group">
+              <button onClick={this.onSubmit} onMouseOver={this.showPopup}>
+                Ok
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     );
   }
 }
+
+const REACT_HOVER_OPTS = {
+  followCursor: true,
+  shiftX: -60,
+  shiftY: 0,
+};
+
+const SEARCH_INPUT_POPUP_TEXT =
+  "Search by city name or by latitude and longitude (lat:lon)";
 
 export default Search;
