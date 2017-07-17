7bfa2d8cd7e9f2a3494f58be03497750e5438059
Maciek <maciejostojski4@gmail.com>
2017-07-17 15:26:37 +0200

    Utworzenie kolumny w widoku szczegółów na dane o obecnym stani pogody

--- src/weather/WeatherDetails.js
+++ src/weather/WeatherDetails.js
@@ -67,7 +67,9 @@ class WeatherDetails extends Component {
     return (
       <div>
         <div className="row">
-          <div className="col-md-4" />
+          <div className="col-md-4" >
+
+          </div>
           <div className="col-md-8">
             <LineChart
               width={730}
