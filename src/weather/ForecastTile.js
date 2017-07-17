3991ed9cc7b2a9eb4e7a4b13f870f97fd217e272
Maciek <maciejostojski4@gmail.com>
2017-07-17 15:07:36 +0200

    Zmienione stylowanie prognozy pogody

--- src/weather/ForecastTile.js
+++ src/weather/ForecastTile.js
@@ -13,64 +13,18 @@ class ForecastTile extends React.Component {
     };
   }
 
-  parseDate = weatherDate => {
-    return weatherDate.split(" ")[0];
-  };
-
-  changeRenderedData = () => {
-    if(this.state.dataToRender.time === 'day') {
-      this.setState({
-        dataToRender: {
-          temp: this.props.nightForecast.main.temp,
-          pressure: this.props.nightForecast.main.pressure,
-          wind: this.props.nightForecast.wind.speed,
-          time: "night",
-          date: this.parseDate(this.props.dayForecast.dt_txt)
-        }
-      });
-    } else {
-      this.setState({
-        dataToRender: {
-          temp: this.props.dayForecast.main.temp,
-          pressure: this.props.dayForecast.main.pressure,
-          wind: this.props.dayForecast.wind.speed,
-          time: "day",
-          date: this.parseDate(this.props.dayForecast.dt_txt)
-        }
-      })
-    }
-  };
-
-  componentDidMount() {
-    this.setState({
-      dataToRender: {
-        temp: this.props.dayForecast.main.temp,
-        pressure: this.props.dayForecast.main.pressure,
-        wind: this.props.dayForecast.wind.speed,
-        time: "day",
-        date: this.parseDate(this.props.dayForecast.dt_txt)
-      }
-    })
-  }
-
   render() {
     return (
-      <Tile className="container" onClick={this.changeRenderedData}>
-        <TitleField className="text-center">
-          <h3>
-            <b>
-              {this.state.dataToRender.date} at {this.state.dataToRender.time}
-            </b>
-          </h3>
-        </TitleField>
+      <Tile style={{"background-color": this.props.backgroundColor}}>
+
         <TileField className="text-center">
-          Temperature: {this.state.dataToRender.temp} &deg;C
+          Temperature: {this.props.forecast.main.temp}&deg;C
         </TileField>
         <TileField className="text-center">
-          Pressure: {this.state.dataToRender.pressure} hPa
+          Pressure: {this.props.forecast.main.pressure}hPa
         </TileField>
         <TileField className="text-center">
-          Wind: {this.state.dataToRender.wind} km/h
+          Wind: {this.props.forecast.wind.speed}km/h
         </TileField>
       </Tile>
     );
@@ -79,25 +33,15 @@ class ForecastTile extends React.Component {
 
 const Tile = styled.div`
   flex: 1;
-  margin: 5px;
+  margin: 10px;
   color: white;
   min-height: 200px;
   min-width: 200px;
   box-shadow: 2px 2px 4px black;
-  background-color: #cddc39;
   padding: 10px;
-
   &:hover {
     opacity: 0.7;
   }
-`;
-
-const TitleField = styled.div`
-  background-color: #33691e;
-  color: white;
-  box-shadow: 2px 2px 4px black;
-  padding: 2px;
-  margin-bottom: 10px;
 `;
 
 const TileField = styled.div`
--- src/weather/WeatherDetails.js
+++ src/weather/WeatherDetails.js
@@ -82,13 +82,26 @@ class WeatherDetails extends Component {
           <Line type="monotone" dataKey="temperature" stroke="#e91b1b" />
         </LineChart>
 
+        <div className="text-center">
+          <h2>Forecast for 4 days</h2>
+        </div>
         <ForecastPlaceHolder>
           {this.state.dayForecast.map((forecast, index) => {
             return (
-              <ForecastTile
-                dayForecast={forecast}
-                nightForecast={this.state.nightForecast[index]}
-              />
+              <DailyForecastPlaceHolder>
+                <DateTile className="text-center">
+                  <h3>
+                    {forecast.dt_txt.split(" ")[0]}
+                  </h3>
+                </DateTile>
+                <h4 className="text-center">Day</h4>
+                <ForecastTile forecast={forecast} backgroundColor="#8bc34a" />
+                <h4 className="text-center">Night</h4>
+                <ForecastTile
+                  forecast={this.state.nightForecast[index]}
+                  backgroundColor="#33691e"
+                />
+              </DailyForecastPlaceHolder>
             );
           })}
         </ForecastPlaceHolder>
@@ -96,6 +109,22 @@ class WeatherDetails extends Component {
     );
   }
 }
+
+const DateTile = styled.div`
+  background-color: #827717;
+  margin: 10px;
+  margin-bottom: 10px;
+  color: white;
+  padding: 2px;
+  box-shadow: 2px 2px 4px black;
+`;
+
+const DailyForecastPlaceHolder = styled.div`
+  flex: 1;
+  background-color: #e6ee9c;
+  margin: 5px;
+  box-shadow: 2px 2px 4px black;
+`;
 
 const ForecastPlaceHolder = styled.div`
   display: flex;
