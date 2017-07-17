512f2c1ee421955089076fcf63fb76df225ce63e
Maciek <maciejostojski4@gmail.com>
2017-07-17 09:57:15 +0200

    Dodane kolory kafelków zależne od stanu pogody

--- src/weather/card/WeatherTile.js
+++ src/weather/card/WeatherTile.js
@@ -3,13 +3,17 @@
  */
 import React from "react";
 import styled from "styled-components";
-import { withRouter } from "react-router";
-import { changeDisplayedDetailsAction } from "../reducer/actions/weather-actions";
-import { connect } from "react-redux";
+import {withRouter} from "react-router";
+import {changeDisplayedDetailsAction} from "../reducer/actions/weather-actions";
+import {connect} from "react-redux";
 
 class WeatherTile extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      tileColor: { "background-color": GOOD_WEATHER_COLOR },
+    };
   }
 
   showDetails = e => {
@@ -18,22 +22,71 @@ class WeatherTile extends React.Component {
   };
 
   getWeatherIcon = () => {
-    return (
-      "http://openweathermap.org/img/w/" +
-      this.props.city.weather[0].icon +
-      ".png"
-    );
+    return ICON_URL + this.props.city.weather[0].icon + ".png";
   };
+
+  refreshTileColorInState = tileColor => {
+    this.setState({
+      tileColor: { "background-color": tileColor },
+    });
+  };
+
+  isBadWeather = weatherCode => {
+    if (
+      weatherCode < 500 ||
+      (weatherCode >= 600 && weatherCode < 700) ||
+      weatherCode >= 900
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  isAverageWeather = weatherCode => {
+    if (
+      (weatherCode >= 500 && weatherCode < 600) ||
+      (weatherCode >= 700 && weatherCode < 800) ||
+      (weatherCode >= 803 && weatherCode <= 804)
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  isGoodWeather = weatherCode => {
+    if (weatherCode >= 800 && weatherCode <= 802) {
+      return true;
+    }
+    return false;
+  };
+
+  resolveTileColor = weatherCode => {
+    if (this.isBadWeather(weatherCode)) {
+      this.refreshTileColorInState(BAD_WEATHER_COLOR);
+    } else if (this.isAverageWeather(weatherCode)) {
+      this.refreshTileColorInState(AVERAGE_WEATHER_COLOR);
+    } else if (this.isGoodWeather(weatherCode)) {
+      this.refreshTileColorInState(GOOD_WEATHER_COLOR);
+    }
+  };
+
+  componentDidMount() {
+    this.resolveTileColor(this.props.city.weather[0].id);
+  }
 
   render() {
     return (
-      <Card className="text-center" onClick={this.showDetails}>
+      <Card
+        className="text-center"
+        onClick={this.showDetails}
+        style={this.state.tileColor}
+      >
         <div className="row">
           <div>
             <CardField>
-              <Title>
+              <TitleField>
                 {this.props.city.name}
-              </Title>
+              </TitleField>
             </CardField>
             <CardField>
               <img
@@ -53,7 +106,17 @@ class WeatherTile extends React.Component {
     );
   }
 }
-const Title = styled.b`font-size: 160%;`;
+
+const GOOD_WEATHER_COLOR = "#cddc39";
+
+const AVERAGE_WEATHER_COLOR = "#7cb342";
+
+const BAD_WEATHER_COLOR = "#2e7d32";
+
+const ICON_URL = "http://openweathermap.org/img/w/";
+
+const TitleField = styled.b`font-size: 160%;`;
+
 const CardField = styled.div`
   flex: 1;
   min-height: 40px;
@@ -65,7 +128,6 @@ const Card = styled.div`
   float: left;
   margin: 5px;
   min-width: 200px;
-  background-color:  #cddc39;
   flex: 1;
   flex-direction: column;
   display: flex;
@@ -73,7 +135,7 @@ const Card = styled.div`
   &:hover {
     opacity: 0.7;
   }
-  &:active{
+  &:active {
     box-shadow: 0px 0px 0px;
   }
 `;
