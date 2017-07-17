883a392ef3416d73bbd483d3ab433429de74047d
deks <deks@MacBook-Air-deks.local>
2017-07-17 15:11:24 +0200

    fixes

--- src/weather/WeatherDetails.js
+++ src/weather/WeatherDetails.js
@@ -9,7 +9,8 @@ import {
   CartesianGrid,
   Tooltip,
   Legend,
-  ResponsiveContainer
+  ResponsiveContainer,
+  ReferenceLine
 } from "recharts";
 
 class WeatherDetails extends Component {
@@ -20,25 +21,21 @@ class WeatherDetails extends Component {
   }
 
   render() {
+    console.log(this.props.data.city.name);
     const forecastData = this.props.data.list
       .map(a => {
         return {
           temperature: a.main.temp,
           time: a.dt_txt.slice(11, 16),
-          day: `${a.dt_txt.slice(8, 10)}.${a.dt_txt.slice(5, 7)}`,
-          td: `${a.dt_txt.slice(11, 16)} ${a.dt_txt.slice(
-            8,
-            10
-          )}.${a.dt_txt.slice(5, 7)}`
+          day: `${a.dt_txt.slice(8, 10)}.${a.dt_txt.slice(5, 7)}`
         };
       })
       .slice(0, 9);
 
     return (
-      <div>
+      /*
+      <ResponsiveContainer width="60%" aspec={2}>
         <LineChart
-          width={730}
-          height={250}
           data={forecastData}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
         >
@@ -50,13 +47,50 @@ class WeatherDetails extends Component {
           <Legend />
           <Line type="monotone" dataKey="temperature" stroke="#e91b1b" />
         </LineChart>
-      </div>
+      </ResponsiveContainer>
+      */
+      <ChartContainer>
+        <h3>
+          Temperatures in next 24 hours in {this.props.data.city.name}
+        </h3>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={forecastData}
+            margin={{ top: 20, right: 30, left: 0, bottom: 0 }}
+          >
+            <XAxis dataKey="time" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip>
+              <Line
+                type="monotone"
+                dataKey="temperature"
+                stroke="#8884d8"
+                fillOpacity={1}
+                fill="url(#colorUv)"
+              />
+            </Tooltip>
+
+            <Line
+              type="monotone"
+              dataKey="temperature"
+              stroke="#e91b1b"
+              fill="#8884d8"
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </ChartContainer>
     );
   }
 }
 
-const Div = styled.div`display: flex;`;
-
+const ChartContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  height: 30vh;
+  text-align: center;
+`;
 const WEATHER_FOR_SINGLE_CITY_URL = "/weather?units=metric&";
 
 const mapStateToProps = currentState => {
