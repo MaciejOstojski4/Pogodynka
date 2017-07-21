e353daae168f0683c9eb3eb4ec0a4ad374a57b18
Maciek <maciejostojski4@gmail.com>
2017-07-21 12:35:22 +0200

    Skonfigurowany redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,14 +1,15 @@
-import { compose, createStore, combineReducers } from "redux";
+import { compose, createStore, combineReducers, applyMiddleware } from "redux";
 import persistState from "redux-localstorage";
 import { weatherReducer } from "./weather/reducer/weather";
 import { sessionReducer } from "./user/reducer/session";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   weather: weatherReducer,
   session: sessionReducer,
 });
 
-const enhancer = compose(persistState("weather"));
+const enhancer = compose(applyMiddleware(thunk), persistState("weather"));
 
 const store = createStore(rootReducer, {}, enhancer);
 
